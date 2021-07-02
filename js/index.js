8dd5f60d2c86373c8d32a8554571ccad1d2aca50
shakhzod2003 <yshahzod0@gmail.com>
2021-07-02 15:22:36 +0500

    First commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$('#carouselOne').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:true,
+    autoplay:true,
+    autoplayTimeout:10000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
